e7640fadbda52f26e80ad3356ad329b1b90706dc
Elson Li <elsonli@Elsons-MacBook-Pro.local>
2019-03-27 20:36:44 -0700

    Updates

---
+++ precache-manifest.c4fc709ebf643244c085d9b8a33c232f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6b25e57a9264eeb2b815",
+    "url": "/static/js/main.e2089540.chunk.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "6b25e57a9264eeb2b815",
+    "url": "/static/css/main.e91807f6.chunk.css"
+  },
+  {
+    "revision": "92aa060bd128e7a6e6464b53a9e859c4",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6389578baa596d4d24b54d8f0184b338.js"
+  "/precache-manifest.c4fc709ebf643244c085d9b8a33c232f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
